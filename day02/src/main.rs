c3b2a6de85e95953ef35f6cfa5ca4aa2ebde466b
Peter Kubiak <peter.s.kubiak@bath.edu>
2024-12-09 10:12:27 +0000

    Complete day 2 part 1

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 
+use colored::Colorize;
 use regex::Regex;
 
 #[derive(PartialEq, Eq)]
@@ -18,30 +19,41 @@ fn main() {
             .collect::<Vec<i32>>()
     });
 
-    let safe_reports = reports.filter(|report| {
-        report
-            .iter()
-            .try_fold((None, None), |state, level| match state {
-                (None, _) => Ok((Some(level), None)),
-                (Some(previous_level), report_direction) => {
-                    let direction = match level - previous_level {
-                        1..=3 => Direction::Up,
-                        -3..=-1 => Direction::Down,
-                        difference => return Err(String::from("difference of {difference}")),
-                    };
-                    if let Some(report_direction) = report_direction {
-                        if direction == report_direction {
-                            Ok((Some(level), Some(direction)))
+    let safe_report_count = reports
+        .filter(|report| {
+            let result = report
+                .iter()
+                .try_fold((None, None), |state, level| match state {
+                    (None, _) => Ok((Some(level), None)),
+                    (Some(previous_level), report_direction) => {
+                        let direction = match level - previous_level {
+                            1..=3 => Direction::Up,
+                            -3..=-1 => Direction::Down,
+                            difference => {
+                                return Err(format!(
+                                    "difference of {difference} at {previous_level} -> {level}"
+                                ))
+                            }
+                        };
+                        if let Some(report_direction) = report_direction {
+                            if direction == report_direction {
+                                Ok((Some(level), Some(direction)))
+                            } else {
+                                Err(format!("direction change at {previous_level} -> {level}"))
+                            }
                         } else {
-                            Err(String::from(
-                                "direction change at {previous_level} -> {level}",
-                            ))
+                            Ok((Some(level), Some(direction)))
                         }
-                    } else {
-                        Ok((Some(level), Some(direction)))
                     }
-                }
-            })
-            .is_ok()
-    });
+                });
+            let trace_message = if let Err(reason) = &result {
+                format!("{}: {}", "UNSAFE".red(), reason)
+            } else {
+                "SAFE".green().to_string()
+            };
+            println!("{report:?}\t{trace_message}");
+            result.is_ok()
+        })
+        .count();
+    println!("{safe_report_count} safe reports found");
 }
