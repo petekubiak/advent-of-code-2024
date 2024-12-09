cb74983c6d5464ee5df20dec4b5532ce12274077
Peter Kubiak <peter.s.kubiak@bath.edu>
2024-12-09 00:41:11 +0000

    Continue day 2 part 1 - adding reports for inspect()

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -2,29 +2,46 @@ use std::fs;
 
 use regex::Regex;
 
+#[derive(PartialEq, Eq)]
+enum Direction {
+    Up,
+    Down,
+}
+
 fn main() {
     let pattern = Regex::new(r"\d+").unwrap();
-    // let first_line = fs::read_to_string("input")
-    //     .unwrap()
-    //     .lines()
-    //     .next()
-    //     .unwrap()
-    //     .to_owned();
-    // println!("{}", first_line);
-    // let caps = pattern.captures_iter(&first_line);
-    // for num in caps {
-    //     println!("{:?}", num);
-    // }
     let input = fs::read_to_string("input").unwrap();
-    let reports = input
-        .lines()
-        .map(|levels| {
-                pattern
-                    .captures_iter(levels)
-                    .map(|capture| capture[0].parse::<i32>().unwrap())
-        });
-    // println!("{:#?}", reports);
+    let reports = input.lines().map(|levels| {
+        pattern
+            .captures_iter(levels)
+            .map(|capture| capture[0].parse::<i32>().unwrap())
+            .collect::<Vec<i32>>()
+    });
 
-    let safe_reports = reports
-        .filter(|levels| )
+    let safe_reports = reports.filter(|report| {
+        report
+            .iter()
+            .try_fold((None, None), |state, level| match state {
+                (None, _) => Ok((Some(level), None)),
+                (Some(previous_level), report_direction) => {
+                    let direction = match level - previous_level {
+                        1..=3 => Direction::Up,
+                        -3..=-1 => Direction::Down,
+                        difference => return Err(String::from("difference of {difference}")),
+                    };
+                    if let Some(report_direction) = report_direction {
+                        if direction == report_direction {
+                            Ok((Some(level), Some(direction)))
+                        } else {
+                            Err(String::from(
+                                "direction change at {previous_level} -> {level}",
+                            ))
+                        }
+                    } else {
+                        Ok((Some(level), Some(direction)))
+                    }
+                }
+            })
+            .is_ok()
+    });
 }
