dcc86d07dd4e659445b3d900d0875a4f5dd2ff2a
Peter Kubiak <peter.s.kubiak@bath.edu>
2024-12-12 08:51:48 +0000

    [WIP] day 2 part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -21,32 +21,11 @@ fn main() {
 
     let safe_report_count = reports
         .filter(|report| {
-            let result = report
-                .iter()
-                .try_fold((None, None), |state, level| match state {
-                    (None, _) => Ok((Some(level), None)),
-                    (Some(previous_level), report_direction) => {
-                        let direction = match level - previous_level {
-                            1..=3 => Direction::Up,
-                            -3..=-1 => Direction::Down,
-                            difference => {
-                                return Err(format!(
-                                    "difference of {difference} at {previous_level} -> {level}"
-                                ))
-                            }
-                        };
-                        if let Some(report_direction) = report_direction {
-                            if direction == report_direction {
-                                Ok((Some(level), Some(direction)))
-                            } else {
-                                Err(format!("direction change at {previous_level} -> {level}"))
-                            }
-                        } else {
-                            Ok((Some(level), Some(direction)))
-                        }
-                    }
-                });
-            let trace_message = if let Err(reason) = &result {
+            let result = check_report(report).map_err(|(reason, position)| {
+                let remove_before = report.clone();
+                remove_before.remove()(reason, position)
+            });
+            let trace_message = if let Err((reason, _)) = &result {
                 format!("{}: {}", "UNSAFE".red(), reason)
             } else {
                 "SAFE".green().to_string()
@@ -56,4 +35,38 @@ fn main() {
         })
         .count();
     println!("{safe_report_count} safe reports found");
+}
+
+fn check_report(report: &Vec<i32>) -> Result<(), (String, usize)> {
+    report
+        .iter()
+        .enumerate()
+        .try_fold((None, None), |state, (position, level)| match state {
+            (None, _) => Ok((Some(level), None)),
+            (Some(previous_level), report_direction) => {
+                let direction = match level - previous_level {
+                    1..=3 => Direction::Up,
+                    -3..=-1 => Direction::Down,
+                    difference => {
+                        return Err((
+                            format!("difference of {difference} at {previous_level} -> {level}"),
+                            position,
+                        ))
+                    }
+                };
+                if let Some(report_direction) = report_direction {
+                    if direction == report_direction {
+                        Ok((Some(level), Some(direction)))
+                    } else {
+                        Err((
+                            format!("direction change at {previous_level} -> {level}"),
+                            position,
+                        ))
+                    }
+                } else {
+                    Ok((Some(level), Some(direction)))
+                }
+            }
+        })
+        .map(|_| ())
 }
