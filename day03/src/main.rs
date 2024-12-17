0cc13230ef2a310e0bf21048476bba7d0acd3d82
Peter Kubiak <peter.s.kubiak@bath.edu>
2024-12-17 09:52:34 +0000

    Nice formatting on day 3

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let sum = pattern
         .captures_iter(&input)
         .fold((0, true), |(sum, enabled), capture| {
-            print!("Found {:10} ", &capture[0]);
+            print!("Found {:15} ", &capture[0]);
             match &capture[0] {
                 "do()" => {
                     println!("{}", "Enabling...".green());
@@ -25,7 +25,7 @@ fn main() {
                             capture[1].parse::<i32>().unwrap() * capture[2].parse::<i32>().unwrap();
                         let new_sum = sum + product;
                         println!(
-                            "{:4} * {:4} = {product}; sum: {new_sum}",
+                            "{:4} * {:4} = {product:8}; sum: {new_sum}",
                             &capture[1], &capture[2]
                         );
                         (new_sum, enabled)
